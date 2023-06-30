b68188acd87b3d443a0e8e3167a124befd35c4cf
ChichitheTechbaddie <Rettaogbu@gmail.com>
2023-06-30 05:26:06 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,23 @@
-c
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-int a[] = { 2015, 2016, 98, 2017, 2018 };
-int n = 98;
-int *p = &n;
+  int n;
+  int a[5];
+  int *p;
 
-p = &a[2];
-printf("a[2] = %d\n", *p); // added line
-
-return 0;
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
